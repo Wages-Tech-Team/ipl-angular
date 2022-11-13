3d45e0f942f1dbc6d016834f9c890fd72c246651
tanvivats13 <tanvi.vats51@gmail.com>
2022-11-13 19:27:17 +0530

    changes for upload and delete payto

--- src/app/add-pay-to/add-pay-to.component.ts
+++ src/app/add-pay-to/add-pay-to.component.ts
@@ -13,6 +13,11 @@ export class AddPayToComponent implements OnInit {
   addpayTo: FormGroup;
   submitted: boolean = false;
   msgs: any;
+  payTo: any = [];
+  heading: any;
+  filename: any;
+  errorMessage: string ='';
+  uploadedFile: any;
 
   constructor(private router: Router, public service: CommonService,private confirmationService: ConfirmationService, private primengConfig: PrimeNGConfig) {
     this.addpayTo = new FormGroup({
@@ -23,6 +28,7 @@ export class AddPayToComponent implements OnInit {
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
+    this.getPayToDropdown();
   }
   navigatedToSignUp() {
     this.router.navigateByUrl('/sign-up');
@@ -31,42 +37,112 @@ export class AddPayToComponent implements OnInit {
     this.router.navigateByUrl('/get-user-details');
   }
 
-  submitPayTo = () => {
-    if (this.addpayTo.invalid) {
-      this.submitted = true;
-      return
-    }
-    this.submitted = false;
-
-    this.service.showloader = true;
+ 
+  getPayToDropdown = () => {
     let body = {
-      "pay_to_name": this.addpayTo.get('pay_to')?.value,
-      "pay_to_code": this.addpayTo.get('pay_to_code')?.value
+      "no_of_records": 1000,
+      "page_no": 1
     }
-    this.service.postRequest("add-pay-details", body).subscribe(res => {
+    this.service.postRequest("get-pay-to-details", body).subscribe(res => {
       if (res.body.success == true || res.body.code == 1000) {
+        this.payTo = res.body.data.pay_to_details;
         this.service.showloader = false;
-        this.router.navigateByUrl('/main'); 
       }
       else {
-      this.service.showloader = false;
-      this.confirm(res.body.message, 'Error');
-    }
+        this.service.showloader = false;
+        this.confirm(res.body.message, 'Error');
+      }
     })
   }
+  
+  setPayToId(value: any) {
+    value = value.pay_to_name
+    if (!value)
+      return
+    this.addpayTo?.get('pay_to_code')?.setValue('');
+    sessionStorage.setItem('payTo', value);
+    for (let detail = 0; detail < this.payTo.length; detail++) {
+      if (value == this.payTo[detail].pay_to_name)
+      this.addpayTo?.get('pay_to_code')?.setValue(this.payTo[detail].pay_to_code);
+    }
+  }
+
+  deletePayTo = (value: any) => {
+    this.service.showloader = true;
+    let body: any;
+        body = {
+          "id": value.id,
+        }
+    this.service.postRequest("delete-pay-details", body).subscribe(res => {
+      if (res.body.success == true  || res.body.code == 1000) {
+        this.service.showloader = false;
+        this.getPayToDropdown();
+        this.confirm('Successfully Deleted !', '');
+      }
+      else {
+        this.service.showloader = false;
+        this.confirm(res.body.message, 'Error');
+      }
+    })
+  }
+  
   reject = () => {
     this.confirmationService.close();
   }
 
   accept = () => {
     this.confirmationService.close();
-    this.msgs = [{ severity: 'info', summary: 'Confirmed', detail: 'You have accepted' }];
+    if(this.heading=='Delete Detail')
+    this.deletePayTo(this.addpayTo?.get('pay_to')?.value);
+    if(this.heading=='Upload Project')
+    this.upload();
+    
   }
 
   confirm(message: any, heading: any) {
+    if(heading=='Delete Detail')
+    this.submitted = true;
+
+    if (!this.filename && heading == 'Upload Project') {
+      this.errorMessage = "Please Upload the file."
+      return
+    }
+
+    if(heading=='Delete Detail' && this.addpayTo?.invalid)
+    return;
+
+    this.heading= heading;
     this.confirmationService.confirm({
       message: message,
       header: heading,
     });
+  }
+  
+  delete() {
+    this.filename = ''
+  }
+  
+  handleFileUpload(event: any) {
+    let files = event.target.files;
+    this.uploadedFile = files[0]
+    this.filename = files[0].name;
+  }
+
+  upload = () => {
+    if (!this.uploadedFile)
+      return;
+
+    this.service.showloader = true;
+    let data = new FormData();
+    data.append("request[file]", this.uploadedFile);
+    this.service.postRequestForUpload("upload-pay-details", data).subscribe(res => {
+      if (res.body.success == true || res.body.code == 1000) {
+        this.service.showloader = false;
+      }
+      else {
+        this.service.showloader = false;
+        this.confirm(res.body.message, 'Error');
+      }
+    })
   }
 }
